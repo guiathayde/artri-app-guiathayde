ef69c07bc5ada2928c053e4a51582ab13e2de036
Vitor de Almeida Recoaro <vitordearec@gmail.com>
2026-05-11 11:31:59 -0300

    refactor: removing custom exercises
    Removing custom exercises from current app, because this exercises is
    not fully implemented.

--- lib/views/physical_exercise/widgets/physical_exercise_home.dart
+++ lib/views/physical_exercise/widgets/physical_exercise_home.dart
@@ -1,11 +1,8 @@
-import 'package:artriapp/routes/index.dart';
 import 'package:artriapp/utils/enums/index.dart';
-import 'package:artriapp/utils/helpers/index.dart';
 import 'package:artriapp/utils/index.dart';
 import 'package:artriapp/view_models/index.dart';
 import 'package:artriapp/views/index.dart';
 import 'package:flutter/widgets.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -43,22 +40,22 @@ class PhysicalExerciseHome extends StatelessWidget {
                 gradientColors: AppGradients.greenToNeutral,
                 buttonText: 'Pés',
               ),
-              Text(
-                'Aqui você terá acesso a exercícios personalizados para diferentes partes do corpo. Clique para escolher o nível de dificuldade e personalizar seus exercícios:',
-                style: GoogleFonts.montserrat(
-                  textStyle: const TextStyle(
-                    fontSize: 24,
-                    color: AppColors.darkGreen,
-                  ),
-                ),
-              ),
-              ExerciseButton(
-                onClick: () =>
-                    context.go(PhysicalExerciseRoutes.customExercises),
-                gradientColors: AppGradients.greenToNeutral,
-                buttonText: 'Personalizados',
-                width: ScreenHelper.getScreenWidth(context) * 0.65,
-              ),
+              // Text(
+              //   'Aqui você terá acesso a exercícios personalizados para diferentes partes do corpo. Clique para escolher o nível de dificuldade e personalizar seus exercícios:',
+              //   style: GoogleFonts.montserrat(
+              //     textStyle: const TextStyle(
+              //       fontSize: 24,
+              //       color: AppColors.darkGreen,
+              //     ),
+              //   ),
+              // ),
+              // ExerciseButton(
+              //   onClick: () =>
+              //       context.go(PhysicalExerciseRoutes.customExercises),
+              //   gradientColors: AppGradients.greenToNeutral,
+              //   buttonText: 'Personalizados',
+              //   width: ScreenHelper.getScreenWidth(context) * 0.65,
+              // ),
             ],
           ),
         );
